eadc22ca73791460bc3344f50c5b4b8fce0b511d
VacantArkade <kadehuskey@gmail.com>
2025-03-31 22:50:33 -0500

    Rangerrang Progress?

---
+++ Assets/Scripts/Guns/Boomerang.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Boomerang : Gun
+{
+    public override bool AttemptFire()
+    {
+        if (!base.AttemptFire())
+            return false;
+
+        var b = Instantiate(bulletPrefab, gunBarrelEnd.transform.position, gunBarrelEnd.rotation);
+        b.GetComponent<Projectile>().Initialize(3, 1, 20, 5, null); // version without special effect
+        //b.GetComponent<Projectile>().Initialize(1, 100, 2, 5, DoThing); // version with special effect
+
+        anim.SetTrigger("shoot");
+        elapsed = 0;
+        ammo -= 1;
+
+        return true;
+    }
+}
